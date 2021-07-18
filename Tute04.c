431bbc7c987fa88749ac30a928474c7c145dd55b
IT21083914 <87597116+IT21083914@users.noreply.github.com>
2021-07-18 12:42:01 +0000

    I have completed Tute04

--- Tute04.c
+++ Tute04.c
@@ -6,6 +6,11 @@ Do not change the code given in the main() function when you are implementing yo
 
 #include <stdio.h>
 
+//function prototypes
+int minimum(int num1 , int num2);
+int maximum(int num1 , int num2);
+int multiply(int num1 , int num2);
+
 int main() {
    int no1, no2;
    printf("Enter a value for no 1 : ");
@@ -16,4 +21,38 @@ int main() {
    printf("%d ", maximum(no1, no2));
    printf("%d ", multiply(no1, no2));
    return 0;
+}
+int minimum(int num1 , int num2)
+{
+	int min; //declaring variable.
+	
+	if(num1 < num2) //check whether the number1 less than number2.
+	{
+		min = num1;
+	}
+	else
+	{
+		min = num2;
+	}//end if
+	
+	return min;
+}
+int maximum(int num1 , int num2)
+{
+	int max; //declaring variable.
+	
+	if(num1 > num2) //check whether the number1 grather than number2.
+	{
+		max = num1;
+	}
+	else
+	{
+		max = num2;
+	}//end if
+	
+	return max;
+}
+int multiply(int num1 , int num2)
+{
+	return num1 * num2;
 }
